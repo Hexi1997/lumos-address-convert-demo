27b9c0b664db0f6def2a426f959e1fed5a4bba9d
Hexi1997 <18715156450@163.com>
2024-07-01 01:52:24 +0800

    feat: support metamask address convert

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { helpers, Script, config, commons } from "@ckb-lumos/lumos";
+import ReactDOM from "react-dom";
+import { asyncSleep, capacityOf, ethereum, transfer } from "./lib";
 
-function App() {
-  const [count, setCount] = useState(0)
+// use testnet config
+config.initializeConfig(config.predefined.LINA);
+
+export function App() {
+  const [ethAddr, setEthAddr] = useState("");
+  const [omniAddr, setOmniAddr] = useState("");
+  const [omniLock, setOmniLock] = useState<Script>();
+  const [balance, setBalance] = useState("-");
+
+  const [transferAddr, setTransferAddress] = useState("");
+  const [transferAmount, setTransferAmount] = useState("");
+
+  const [isSendingTx, setIsSendingTx] = useState(false);
+  const [txHash, setTxHash] = useState("");
+
+  useEffect(() => {
+    asyncSleep(100).then(() => {
+      if (ethereum.selectedAddress) connectToMetaMask();
+      ethereum.addListener("accountsChanged", connectToMetaMask);
+    });
+  }, []);
+
+  function connectToMetaMask() {
+    ethereum
+      .enable()
+      .then(([ethAddr]: string[]) => {
+        const omniLockScript = commons.omnilock.createOmnilockScript({ auth: { flag: "ETHEREUM", content: ethAddr } });
+
+        const omniAddr = helpers.encodeToAddress(omniLockScript);
+
+        setEthAddr(ethAddr);
+        setOmniAddr(omniAddr);
+        setOmniLock(omniLockScript);
+
+        return omniAddr;
+      })
+      .then((omniAddr) => capacityOf(omniAddr))
+      .then((balance) => setBalance(balance.div(10 ** 8).toString() + " CKB"));
+  }
+
+  function onTransfer() {
+    if (isSendingTx) return;
+    setIsSendingTx(true);
+
+    transfer({ amount: transferAmount, from: omniAddr, to: transferAddr })
+      .then(setTxHash)
+      .catch((e) => {
+        console.log(e);
+        alert(e.message || JSON.stringify(e));
+      })
+      .finally(() => setIsSendingTx(false));
+  }
+
+  if (!ethereum) return <div>MetaMask is not installed</div>;
+  if (!ethAddr) return <button onClick={connectToMetaMask}>Connect to MetaMask</button>;
 
   return (
-    <>
+    <div>
+      <ul>
+        <li>Ethereum Address: {ethAddr}</li>
+        <li>Nervos Address(Omni): {omniAddr}</li>
+        <li>
+          Current Omni lock script:
+          <pre>{JSON.stringify(omniLock, null, 2)}</pre>
+        </li>
+
+        <li>Balance: {balance}</li>
+      </ul>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h2>Transfer to</h2>
+        <label htmlFor="address">Address</label>&nbsp;
+        <input id="address" type="text" onChange={(e) => setTransferAddress(e.target.value)} placeholder="ckt1..." />
+        <br />
+        <label htmlFor="amount">Amount</label>
+        &nbsp;
+        <input id="amount" type="text" onChange={(e) => setTransferAmount(e.target.value)} placeholder="shannon" />
+        <br />
+        <button onClick={onTransfer} disabled={isSendingTx}>
+          Transfer
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Tx Hash: {txHash}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+const app = document.getElementById("root");
+ReactDOM.render(<App />, app);
